03f01b2c3197156e71aa5a9bb3bd57addbef01db
Vighnesh Manjrekar <vighneshmanjrekar4193@gmail.com>
2023-11-06 03:01:56 +0530

    Add expeirence, achievement and update about,skills, work

--- script.js
+++ script.js
@@ -28,3 +28,28 @@ document.onkeydown = function (e) {
         return false;
     }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const filterButtons = document.querySelectorAll(".filter-btn");
+  const skillImages = document.querySelectorAll("#skill-div img");
+
+  filterButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      filterButtons.forEach(function (btn) {
+        btn.classList.remove("active");
+      });
+      button.classList.add("active");
+      var filter = button.getAttribute("data-filter");
+
+      skillImages.forEach(function (image) {
+        var imageFilter = image.getAttribute("data-filter");
+
+        if (filter === "all" || filter === imageFilter) {
+          image.style.display = "inline-block";
+        } else {
+          image.style.display = "none";
+        }
+      });
+    });
+  });
+});
